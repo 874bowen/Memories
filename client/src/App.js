0a7b7785930499ef80cc2a89f558b76f79b17ec2
Ivan Bowen <bowenivan16@gmail.com>
2022-12-09 08:32:11 +0300

    redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getPosts } from './actions/posts'
+import Form from './components/Form/Form';
+import Navbar from './components/Navbar/Navbar';
+import Posts from './components/Post/Posts';
 import './index.css';
 
 const App = () => {
+   const dispatch = useDispatch();
+   
+   useEffect(() => {
+      dispatch(getPosts())
+   }, [dispatch]);
+
+
    return (
-      <div>
-         <h1 className="text-3xl font-bold underline">App</h1>
+      <div className='w-full flex justify-center items-center'>
+         <div className='items-center w-[80%] '>
+         <Navbar />
+         <div className='mt-5 py-20'>
+            <div className='mt-5 grid md:grid-cols-3'>
+            <div className='col-span-2'><Posts /></div>
+            <Form />
+            </div>
+         </div>
+         </div>
       </div>
    );
 }

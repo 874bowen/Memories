e1b8b39c1ff50f8a7120138ea1d5817cadcc5336
Ivan Bowen <bowenivan16@gmail.com>
2022-12-10 10:48:07 +0300

    update

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
-const CONNECTION_URL = 'mongodb+srv://Bowen:Bowen123@cluster0.91xxwuf.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })

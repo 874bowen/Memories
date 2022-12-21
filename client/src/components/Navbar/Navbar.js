351d82ef5f9fce6a25befcba6862c5c15e947a73
Ivan Bowen <bowenivan16@gmail.com>
2022-12-21 19:45:17 +0300

    update Navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,17 +4,11 @@ const Navbar = () => {
    return (
       <div className="fixed w-[80%] h-20 shadow-xl">
 			<div className="shadow-lg hover:shadow-[#a8e2e7] rounded-xl flex justify-center items-center h-full">
-				{/* <image
-					src={logo}
-					alt="/"
-					width="70"
-					height="70"
-				/> */}
 				<div>
 					<ul className="items-center xs:gap-1 gap-3 flex">
 						<ul href="/">
 							<li className="text-lg text-[#5EBEC4] center uppercase hover:border-b">
-								<p >Memories 🥰</p>
+								<p>Memories 🥰</p>
 							</li>
 						</ul>
 					</ul>
